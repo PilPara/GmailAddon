c78ac08a76773c6f31a25537dd590796c73b748c
PilPara <avidaniv0@gmail.com>
2026-02-11 13:09:23 +0200

    Fixed env var bug

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, jsonify
 from auth_analyzer import anaylze_auth
+import os
 
 app = Flask(__name__)
 
@@ -14,7 +15,7 @@ def analyze_email():
     return jsonify(response)
 
 if __name__ == "__main__":
-    port = int(os.eviron.get("PORT", 8080))
+    port = int(os.environ.get("PORT", 8080))
     app.run(host="0.0.0.0", port=port, debug=True)
 
 
